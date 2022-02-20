565e847f7af6caa0849c05ab54c8206a6b99a8ff
Lynn Nguyen <AmandaN22@outlook.com>
2022-02-19 17:37:57 -0700

    set state and render restaurant from api

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,29 @@ import * as React from 'react';
 import './styles/App.scss';
 import { getAllRestaurants } from './apiCalls';
 import Header from './Components/Header/Header';
-import { Restaurant, State } from './types';
+import { State } from './types';
 
 
 class App extends React.Component  {
  state: State = {
-   restaurants: [1, 2, 3]
- }
+   restaurants: []
+   }
 
   componentDidMount() {
     getAllRestaurants()
+    .then(data => {
+      console.log(data)
+      this.setState({ restaurants: data.businesses})
+      console.log(this.state)
+    })
   }
-
+  
   render() {
+    const name = this.state.restaurants.length > 0 && <p>{this.state.restaurants[0].name}</p>
+
     return (
       <div className="App">
-        { this.state.restaurants[0] }
+        {name}
         <Header />
       </div>
     );

787527589bb2936d4c41e74715c6d16406000c65
Jani Palomino <palomino.jani@gmail.com>
2022-02-21 03:25:35 -0700

    Style Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,3 +1,23 @@
-const Footer = () => {}
+import * as React from 'react';
+import './Footer.scss'
+// which react router version are we installing?
+// import { NavLink } from 'react-router-dom';
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer className='hidden-desktop'>
+      <section className='footer-home'>
+        <h4>
+          <a href='/' className='black-text'>Home</a>
+        </h4>
+      </section>
+      <section className='footer-lists'>
+        <h4>
+          <a href='/MyLists' className='white-text'>My Lists</a>
+        </h4>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer;

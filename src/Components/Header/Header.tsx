26d0c8037c0b11f045855b74b725acdb86dbdfd3
Jani Palomino <palomino.jani@gmail.com>
2022-02-21 01:48:55 -0700

    Setup Header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 import './Header.scss'
+// which react router version are we installing?
+// import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <p>Hi</p>
+    <header>
+      <section className='logo'>
+        <h1 onClick={() => {window.location.href='/'}}> Lynn's List</h1>
+      </section>
+      <nav>
+          <a className='home' href='/'>Home</a>
+          {/* <NavLink to='/'>Home</NavLink> */}
+          <a href='/MyLists'>My Lists</a>
+          {/* <NavLink to='/MyLists'>My Lists</NavLink> */}
+      </nav>
+    </header>
   )
 }
 

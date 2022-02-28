ed101a97d8cfde3c570565e49f93b2facb5e4f3c
Ali Roemhildt <aliroemhildt@gmail.com>
2022-02-27 20:41:50 -0700

    Adjust dropdown styling

--- src/Components/RestaurantCards/HomeCard.tsx
+++ src/Components/RestaurantCards/HomeCard.tsx
@@ -29,6 +29,10 @@ class HomeCard extends React.Component<Props, State> {
     dropdownOpen: false
   }
 
+  componentDidUpdate = () => {
+    window.addEventListener('scroll', this.closeDropdown);
+  }
+
   displayAddress = (address: string[]) => {
     return address.map((element, index) => 
       <p key={index} className='address'>{element}</p>
@@ -39,6 +43,10 @@ class HomeCard extends React.Component<Props, State> {
     this.setState(prevState => ({ 
       dropdownOpen: !prevState.dropdownOpen
     }))
+  }
+
+  closeDropdown = () => {
+    this.setState({ dropdownOpen: false })
   }
 
   handleClick = (key: string) => {

d5143684a316a1b1f7fce5a0b3f13d4f511ec2a8
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-15 22:42:53 -0600

    Move attrs to own line for readability

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,18 +7,42 @@ import { NavLink } from 'react-router-dom';
 const Header: React.FC = () => {
   return (
     <header>
-      <section className='logo-container' tabIndex={0} onClick={() => {window.location.href='/'}}>
-          <img className='logo hidden-mobile' src={lynnslist_logo} alt="Lynn's List"/> 
-          <img className='mobile-logo hidden-desktop' src={lynnslist_logo_mobile} alt="Lynn's List"/> 
-          <h1 className='hidden-mobile hidden-desktop'>Lynn's List</h1>
+      <section
+        className='logo-container'
+        tabIndex={0}
+        onClick={() => {window.location.href='/'}}
+      >
+        <img
+          className='logo hidden-mobile'
+          src={lynnslist_logo}
+          alt="Lynn's List"
+        /> 
+        <img
+          className='mobile-logo hidden-desktop'
+          src={lynnslist_logo_mobile}
+          alt="Lynn's List"
+        /> 
+        <h1 className='hidden-mobile hidden-desktop'>Lynn's List</h1>
       </section>
       <nav>
         <ul className='menu'>
           <li>
-            <NavLink exact to='/' activeClassName='selected' id="home-nav-button">Home</NavLink>
+            <NavLink
+              exact to='/'
+              activeClassName='selected'
+              id="home-nav-button"
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink exact to='/gottago' activeClassName='selected' id="my-lists-nav">My Lists</NavLink>
+            <NavLink
+              exact to='/gottago'
+              activeClassName='selected'
+              id="my-lists-nav"
+            >
+              My Lists
+            </NavLink>
           </li>
         </ul>
       </nav>

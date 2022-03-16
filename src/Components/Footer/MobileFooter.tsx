5c84300d88bc4504e56f6347260daa4bc0ce49d1
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-15 22:37:09 -0600

    Move attributes to their own line for readability

--- src/Components/Footer/MobileFooter.tsx
+++ src/Components/Footer/MobileFooter.tsx
@@ -6,8 +6,20 @@ const MobileFooter: React.FC = () => {
   return (
     <footer className='hidden-desktop'>
       <section className='mobile-nav'>
-        <NavLink exact to='/' className='nav-item home' activeClassName='selected'>Home</NavLink>
-        <NavLink exact to='/gottago' className='nav-item lists' activeClassName='selected'>My Lists</NavLink>
+        <NavLink
+          exact to='/'
+          className='nav-item home'
+          activeClassName='selected'
+        >
+          Home
+        </NavLink>
+        <NavLink
+          exact to='/gottago'
+          className='nav-item lists'
+          activeClassName='selected'
+        >
+          My Lists
+        </NavLink>
       </section>
     </footer>
   );

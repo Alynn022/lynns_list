d6eed94d8aa97d93a321a7df7dcf7bef5af0e3db
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-15 22:56:51 -0600

    Move attrs to own line to increase readability

--- src/Components/RestaurantCards/ListCard.tsx
+++ src/Components/RestaurantCards/ListCard.tsx
@@ -24,24 +24,55 @@ const displayAddress = (address: string[]) => {
 const ListCard: React.FC<Props> = (props: Props) => {
   return (
     <section className='list-card'>
-      <img className='thumbnail' src={props.image} alt={`${props.name}`} tabIndex={0}/>
+      <img
+        className='thumbnail'
+        src={props.image}
+        alt={`${props.name}`}
+        tabIndex={0}
+      />
       <article className='restaurant-info'>
         <h2 className='name' tabIndex={0}>{props.name}</h2>
         <p className='rating' tabIndex={0}>Rating: {props.rating}</p>
         <p className='phone-number'>
-          <a href={`tel:${props.phone}`} className='phone-link' aria-label='phone number'>{props.phone}</a>
+          <a
+            href={`tel:${props.phone}`}
+            className='phone-link'
+            aria-label='phone number'
+          >
+            {props.phone}
+          </a>
         </p>
         <div className='address-container' tabIndex={0}>
-          {displayAddress(props.location)}
+          { displayAddress(props.location) }
         </div>
       </article>
       <article className='card-buttons'>
-        <button className='delete-button' id={props.id} onClick={() => props.removeFromList(props.selectedList, props.id)}>Remove From List
+        <button
+          className='delete-button'
+          id={props.id}
+          onClick={() => props.removeFromList(props.selectedList, props.id)}
+        >
+          Remove From List
         </button>
-        <a className='yelp-link' href={props.url} target='_blank' tabIndex={-1} rel='noreferrer'>
-          <button className='more-info'>View On Yelp
-            <img src={yelp_icon_white} alt='visit yelp page' className='yelp-logo-white'/>
-            <img src={yelp_icon_black} alt='visit yelp page' className='yelp-logo-black'/>
+        <a
+          className='yelp-link'
+          href={props.url}
+          target='_blank'
+          tabIndex={-1}
+          rel='noreferrer'
+        >
+          <button className='more-info'>
+            View On Yelp
+            <img
+              src={yelp_icon_white}
+              alt='visit yelp page'
+              className='yelp-logo-white'
+            />
+            <img
+              src={yelp_icon_black}
+              alt='visit yelp page'
+              className='yelp-logo-black'
+            />
           </button>
         </a>
       </article>

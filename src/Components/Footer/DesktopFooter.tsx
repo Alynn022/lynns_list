0bcc5dd5262452003d6535e1e40292c2a47621d7
Ali Roemhildt <aliroemhildt@gmail.com>
2022-03-15 22:28:23 -0600

    Move a tag attributes to thier own line for better code readability

--- src/Components/Footer/DesktopFooter.tsx
+++ src/Components/Footer/DesktopFooter.tsx
@@ -19,21 +19,63 @@ const DesktopFooter: React.FC = () => {
           <h3 tabIndex={0}>Follow Us</h3>
           <section className='repo'>
               <h4 tabIndex={0}>Project Repo</h4>
-            <a href='https://github.com/aliroemhildt/lynns_list' target='_blank' rel='noreferrer'>github.com/Alynn022/lynns_list</a>
+            <a
+              href='https://github.com/aliroemhildt/lynns_list'
+              target='_blank'
+              rel='noreferrer'
+            >github.com/Alynn022/lynns_list</a>
           </section>
           <section className='github'>
-              <h4 tabIndex={0}>GitHub Profiles</h4>
-              <a href='https://github.com/ALynn022' target='_blank' rel='noreferrer'>Alynn022</a> |  
-              <a href='https://github.com/janitastic' target='_blank' rel='noreferrer'>Janitastic</a> |  
-              <a href='https://github.com/Daniel-OC' target='_blank' rel='noreferrer'>Daniel-OC</a> |
-              <a href='https://github.com/aliroemhildt' target='_blank' rel='noreferrer'>AliRoemhildt</a>
+            <h4 tabIndex={0}>GitHub Profiles</h4>
+            <a
+              href='https://github.com/ALynn022'
+              target='_blank'
+              rel='noreferrer'
+            >Alynn022</a>
+            |  
+            <a
+              href='https://github.com/janitastic'
+              target='_blank'
+              rel='noreferrer'
+            >Janitastic</a>
+            |  
+            <a
+              href='https://github.com/Daniel-OC'
+              target='_blank'
+              rel='noreferrer'
+            >Daniel-OC</a>
+            |
+            <a
+              href='https://github.com/aliroemhildt'
+              target='_blank'
+              rel='noreferrer'
+            >AliRoemhildt</a>
           </section>
           <section className='linkedin'>
             <h4>LinkedIn</h4>
-            <a href='https://www.linkedin.com/in/lynnnguyen22/' target='_blank' rel='noreferrer'>Lynn Nguyen</a> |
-            <a href='https://www.linkedin.com/in/janipalomino/' target='_blank' rel='noreferrer'>Jani Palomino</a> | 
-            <a href='https://www.linkedin.com/in/daniel-o-connell-maker/' target='_blank' rel='noreferrer'>Daniel O'Connell</a> | 
-            <a href='https://www.linkedin.com/in/aliroemhildt/' target='_blank' rel='noreferrer'>Ali Roemhildt</a>
+            <a
+              href='https://www.linkedin.com/in/lynnnguyen22/'
+              target='_blank'
+              rel='noreferrer'
+            >Lynn Nguyen</a>
+            |
+            <a
+              href='https://www.linkedin.com/in/janipalomino/'
+              target='_blank'
+              rel='noreferrer'
+            >Jani Palomino</a>
+            | 
+            <a
+              href='https://www.linkedin.com/in/daniel-o-connell-maker/'
+              target='_blank'
+              rel='noreferrer'
+            >Daniel O'Connell</a>
+            | 
+            <a
+              href='https://www.linkedin.com/in/aliroemhildt/'
+              target='_blank'
+              rel='noreferrer'
+            >Ali Roemhildt</a>
           </section>
         </div>
       </div>
